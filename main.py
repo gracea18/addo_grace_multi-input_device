dbe1cf6a3fbeb79cf4f07b0feddd458d9a90ac4e
gracea18 <72771341+gracea18@users.noreply.github.com>
2021-01-21 00:54:10 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 while True:
     music.set_volume(255)
     if input.sound_level() > 50 and input.light_level() < 6:
-        music.power_up.play()
+        light.set_all(light.rgb(255, 255, 255))
+        light.clear()
+        light.set_all(light.rgb(255, 255, 255))
         music.siren.play_until_done()
-        light.set_all(light.rgb(255, 0, 0))
-        pause(100)
-        light.set_all(light.rgb(255, 0, 0))
+        
+    
     else:
-        light.set_all(light.rgb(255,255,255))
+        light.show_animation(light.rainbowAnimation, 5000)
--- main.ts
+++ main.ts
@@ -1,13 +1,12 @@
 while (true) {
     music.setVolume(255)
     if (input.soundLevel() > 50 && input.lightLevel() < 6) {
-        music.powerUp.play()
-        music.siren.playUntilDone()
-        light.setAll(light.rgb(255, 0, 0))
-        pause(100)
-        light.setAll(light.rgb(255, 0, 0))
-    } else {
         light.setAll(light.rgb(255, 255, 255))
+        light.clear()
+        light.setAll(light.rgb(255, 255, 255))
+        music.siren.playUntilDone()
+    } else {
+        light.showAnimation(light.rainbowAnimation, 5000)
     }
     
 }
