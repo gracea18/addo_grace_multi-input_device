a94d893b725285f677832f135926dc29d1748f3e
gracea18 <72771341+gracea18@users.noreply.github.com>
2021-01-21 00:40:01 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 while True:
     music.set_volume(255)
-    if input.sound_level() > 50 and input.light_level() > 6:
+    if input.sound_level() > 50 and input.light_level() < 6:
+        music.power_up.play()
         music.siren.play_until_done()
-        light.set_all(light.rgb(255, 255, 255))
-    #if the light level is above 6, set all the neopixels to light yellow and play the wawawawaa sound
-    elif input.light_level() > 6:
-        music.wawawawaa.play_until_done()
-        light.set_all(light.rgb(255, 255, 75))
-    
-
+        light.set_all(light.rgb(255, 0, 0))
+        pause(100)
+        light.set_all(light.rgb(255, 0, 0))
+    else:
+        light.set_all(light.rgb(255,255,255))
--- main.ts
+++ main.ts
@@ -1,12 +1,13 @@
 while (true) {
     music.setVolume(255)
-    if (input.soundLevel() > 50 && input.lightLevel() > 6) {
+    if (input.soundLevel() > 50 && input.lightLevel() < 6) {
+        music.powerUp.play()
         music.siren.playUntilDone()
+        light.setAll(light.rgb(255, 0, 0))
+        pause(100)
+        light.setAll(light.rgb(255, 0, 0))
+    } else {
         light.setAll(light.rgb(255, 255, 255))
-    } else if (input.lightLevel() > 6) {
-        // if the light level is above 6, set all the neopixels to light yellow and play the wawawawaa sound
-        music.wawawawaa.playUntilDone()
-        light.setAll(light.rgb(255, 255, 75))
     }
     
 }
